e4e882110be8742ff1b63802c418ac47e8735c90
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-10-28 07:22:13 -0300

    init example wit transport client v6

---
+++ elastic-transport/src/main/java/com/github/fabiomqs/elastic/ElasticTransportApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticTransportApplication.class, args);
+	}
+
+}
---
+++ elastic-transport/src/test/java/com/github/fabiomqs/elastic/ElasticTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticTransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
